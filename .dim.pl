dc6f88bbcc75e5a26199944c89969812e621ad1a
soh335 <sugarbabe335@gmail.com>
2011-10-19 02:46:31 +0900

    update .dim.pl

--- .dim.pl
+++ .dim.pl
@@ -1,7 +1,7 @@
 # This file is managed by /Users/soh/perl5/perlbrew/perls/perl-5.12.2/bin/dim.
 {
-  dist => 'Data-SpatioTemporalHash',
-  distdir => 'Data-SpatioTemporalHash',
-  module => 'Data::SpatioTemporalHash',
+  dist => 'Data-Wheren',
+  distdir => 'Data-Wheren',
+  module => 'Data::Wheren',
   template => 'Default'
 }
